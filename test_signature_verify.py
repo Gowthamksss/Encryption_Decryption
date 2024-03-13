7b369838e9361c97cb63d2841e3d80487e71bec9
Gowtham <161267515+Gowthamksss@users.noreply.github.com>
2024-03-13 11:02:58 +0530

    Update test_signature_verify.py

--- test_signature_verify.py
+++ test_signature_verify.py
@@ -1,25 +1,35 @@
+import pytest
 import requests
 
+@pytest.fixture
+def file_payload():
+    # Setup code to prepare files payload
+    with open('samplefiles/123.txt', 'rb') as file:
+        files = {
+            'text': ('signature.txt', file, 'text/plain'),
+            'signature': (None, 'MEYCIQDDkNyp7zVz0uj68QtYMv55Jvik784XzKr5QceOCa7FBgIhALnTksHTWBxXTv4MElurJgS9RsFlV7rJ2ugyS4mRkUCD'),
+        }
+        yield files
+        # No specific teardown needed here since 'with' handles file closure
 
-#url = 'https://d2hwrs8h4jws8k.cloudfront.net/signature/verify/'
-url = 'https://api.purecipher.com/signature/verify'
+def test_signature_verification_api(file_payload):
+    # URL and headers setup
+    url = 'https://api.purecipher.com/signature/verify'
+    api_key = 'YFTMeUIB78LtBbM4gC3jnM3qlYhqwfo0'
+    headers = {
+        'accept': 'application/json',
+        'api-key': api_key,
+    }
 
-api_key = 'YFTMeUIB78LtBbM4gC3jnM3qlYhqwfo0'
+    # Send the POST request
+    response = requests.post(url, headers=headers, files=file_payload)
 
-headers = {
-    'accept': 'application/json',
-    'api-key': api_key,
-}
-files = {
-    'text': ('signature.txt', open('samplefiles/123.txt', 'rb'), 'text/plain'),
-    'signature': (None, 'MEYCIQDDkNyp7zVz0uj68QtYMv55Jvik784XzKr5QceOCa7FBgIhALnTksHTWBxXTv4MElurJgS9RsFlV7rJ2ugyS4mRkUCD'),
-}
-response = requests.post(url, headers=headers, files=files)
-files['text'][1].close()
+    # Assertions to validate the response
+    assert response.status_code == 200, f"Error: Status code {response.status_code}"
 
-if response.status_code == 200:
+    # Print the response (assuming JSON format for successful requests)
+    response_json = response.json()
     print("Success:")
-    print(response.json())
-else:
-    print(f"Error: Status code {response.status_code}")
-    print(response.text)
+    print(response_json)
+
+    # Additional assertions can be added here based on expected response content
